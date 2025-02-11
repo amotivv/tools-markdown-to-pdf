8039c0ab770346d4f78a4596236a5650219e4300
Jason Smith <jason@amotivv.com>
2025-02-11 09:24:32 -0500

    fix assets

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,17 +5,5 @@ import netlify from '@astrojs/netlify';
 export default defineConfig({
   integrations: [tailwind()],
   output: 'server',
-  adapter: netlify(),
-  vite: {
-    build: {
-      assetsInlineLimit: 0,
-      rollupOptions: {
-        output: {
-          assetFileNames: 'assets/[name][extname]',
-          chunkFileNames: 'assets/[name].js',
-          entryFileNames: 'assets/[name].js'
-        }
-      }
-    }
-  }
+  adapter: netlify()
 });
